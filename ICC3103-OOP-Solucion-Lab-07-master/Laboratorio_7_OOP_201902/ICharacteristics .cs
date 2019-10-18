67b94d6335f6ce62ca86a9ed4ec78a13ed7b28d9
Joaquin Barrientos <joaquinbarrientos@MacBook-Pro-de-Joaquin-4.local>
2019-10-18 09:40:44 -0300

    Agregado ICharacteristics como interfaz y aplicada a algunas clases

---
+++ ICC3103-OOP-Solucion-Lab-07-master/Laboratorio_7_OOP_201902/ICharacteristics .cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Laboratorio_7_OOP_201902
+{
+    interface ICharacteristics  
+    {
+        public List<string> GetCharacteristics()
+        {
+       
+        }
+    }
+}
