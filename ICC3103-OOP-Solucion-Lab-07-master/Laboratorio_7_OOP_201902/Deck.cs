3ddfb0ee6a1608df4c7b8ef48a7f3d9f7eb75b7b
Joaquin Barrientos <joaquinbarrientos@MacBook-Pro-de-Joaquin-4.local>
2019-10-20 22:04:36 -0300

    Updates for second part

--- ICC3103-OOP-Solucion-Lab-07-master/Laboratorio_7_OOP_201902/Deck.cs
+++ ICC3103-OOP-Solucion-Lab-07-master/Laboratorio_7_OOP_201902/Deck.cs
@@ -46,12 +46,65 @@ namespace Laboratorio_7_OOP_201902
         
         public List<string> GetCharacteristics()
         {
-            //List<Card> Cartas;
-
-            //List<Card> MeleeCards = Cards.Where(Cards.card.Type = EnumType.melee);
-            //int totalCartasMelee = MeleeCards.Count;
            
+            
+                List<string> totalCards = new List<string>();
 
-        }
+                int n = cards.Count();
+                string nTotalOfCards = Convert.ToString(n);
+
+
+                IEnumerable<Card> totalCartasMelee =                 
+                    from card in cards
+                    where card.Type == EnumType.melee
+                    select card;
+
+                IEnumerable<CombatCard> cardMelee =                    
+                    from card in totalCartasMelee
+                    select card as CombatCard;
+
+                IEnumerable<int> totalAttackPonitsMelee =
+                    from card in cardMelee
+                    select card.AttackPoints;
+
+
+                IEnumerable<Card> totalCartasRange =                
+                    from card in cards
+                    where card.Type == EnumType.range
+                    select card;
+
+                IEnumerable<CombatCard> cardRange =
+                    from card in totalCartasRange
+                    select card as CombatCard;
+
+                IEnumerable<int> totalAttackPonitsRange =
+                    from card in cardRange
+                    select card.AttackPoints;
+
+                IEnumerable<Card> totalCartasLongRange =                 
+                    from card in cards
+                    where card.Type == EnumType.longRange
+                    select card;
+
+                IEnumerable<CombatCard> cardLongRange =
+                    from card in totalCartasLongRange
+                    select card as CombatCard;
+
+                IEnumerable<int> totalAttackPonitsLongRange =
+                    from card in cardLongRange
+                    select card.AttackPoints;
+
+                IEnumerable<Card> totalCartasBuff =                 
+                    from card in cards
+                    where card.Type == EnumType.buff
+                    select card;
+
+                IEnumerable<Card> totalCartasWeather =                
+                    from card in cards
+                    where card.Type == EnumType.weather
+                    select card;
+
+
+            }
     }
 }
